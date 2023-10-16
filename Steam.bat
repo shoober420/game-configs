47a9230740108fb27760cdfd3eb088feab2d0412
Vincent Grande <shoober420@gmail.com>
2023-10-16 00:00:18 -0700

    Create Steam.bat

---
+++ Steam.bat
@@ -0,0 +1,2 @@
+cd "C:\Program Files (x86)\Steam"
+start steam.exe -dev -console -nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -cef-disable-sandbox -disable-winh264 -no-cef-sandbox -vrdisable -cef-disable-breakpad -cef-disable-d3d11 +open steam://open/minigameslist
