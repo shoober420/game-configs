112a1ebec1f34b62d5f2c14fbd6b76fe878a934e
Vincent Grande <shoober420@gmail.com>
2023-10-16 00:27:08 -0700

    Update Steam.bat

--- Steam.bat
+++ Steam.bat
@@ -1,4 +1,5 @@
 rem -cef-force-32bit breaks launch
+rem bad idea commands to use: -dev -console -cef-disable-sandbox -no-cef-sandbox -cef-disable-d3d11
 
 cd "C:\Program Files (x86)\Steam"
-start steam.exe -dev -console -nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -cef-disable-sandbox -disable-winh264 -no-cef-sandbox -vrdisable -cef-disable-breakpad -cef-disable-d3d11 +open steam://open/minigameslist
+start steam.exe -nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -disable-winh264 -vrdisable -cef-disable-breakpad +open steam://open/minigameslist
