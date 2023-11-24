85dfde1cfb0975c7f0f99687939f53a54a419a47
Vincent Grande <shoober420@gmail.com>
2023-11-23 16:31:09 -0800

    Update DeleteHLHDContent.bat

--- Half-Life/DeleteHLHDContent.bat
+++ Half-Life/DeleteHLHDContent.bat
@@ -1,5 +1,5 @@
-### Delete Half-Life, Opposing Force, and Blue-Shift HD Content
-### Forces original content for all Half-Life games and expansions
+rem Delete Half-Life, Opposing Force, and Blue-Shift HD Content
+rem Forces original content for all Half-Life games and expansions
 
 rmdir /s /q "C:\Program Files (x86)\Steam\steamapps\common\Half-Life\bshift_hd"
 rmdir /s /q "C:\Program Files (x86)\Steam\steamapps\common\Half-Life\gearbox_hd"
