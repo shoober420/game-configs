ce956a3f39576de71beeb2e8cbdf006d7cac5b24
shoober420 <shoober420@gmail.com>
2024-02-24 22:03:26 -0800

    Update Counter-Strike 1.6

---
+++ Counter-Strike 1.6/CS1.6ReadOnlyConfig.bat
@@ -0,0 +1,26 @@
+REM Makes config files READ ONLY
+REM Prevents admin slowhacking
+
+REM For clean configs:
+REM 1. delete config.cfg, userconfig.cfg, autoexec.cfg, and tempdecal.wad
+REM 2. add new userconfig.cfg and autoexec.cfg
+REM 3. run game so that settings are loaded from userconfig.cfg to new clean config.cfg
+REM 4. finally run this batch script to prevent any other external modifications to config files
+
+cd C:\Program Files (x86)\Steam\steamapps\common\Half-Life\cstrike
+
+del tempdecal.wad
+echo. 2>tempdecal.wad
+attrib +r tempdecal.wad
+
+del autoexec.cfg
+echo. 2>autoexec.cfg
+attrib +r autoexec.cfg
+
+attrib +r config.cfg
+attrib +r autoexec.cfg
+attrib +r userconfig.cfg
+attrib +r userconfighq.cfg
+attrib +r userconfigperf.cfg
+
+PAUSE
