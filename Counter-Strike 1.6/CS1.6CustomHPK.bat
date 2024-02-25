0cc64580a6b6fbdb6568bcf92f0dfd930db9582f
shoober420 <shoober420@gmail.com>
2024-02-25 03:36:27 -0800

    Update CS1.6CustomHPK.bat

--- Counter-Strike 1.6/CS1.6CustomHPK.bat
+++ Counter-Strike 1.6/CS1.6CustomHPK.bat
@@ -1,6 +1,6 @@
 REM Deletes custom.hpk file in cstrike folder and makes it READ-ONLY
 
-cd C:\Program Files (x86)\Steam\steamapps\common\Half-Life\cstrike
+cd "C:\Program Files (x86)\Steam\steamapps\common\Half-Life\cstrike"
 
 del custom.hpk
 echo. 2>custom.hpk
