7b623e7c13d05df0790eaff00456b89d271cc479
shoober420 <shoober420@gmail.com>
2024-02-25 00:56:12 -0800

    Update CS1.6TempDecalWAD.bat

--- Counter-Strike 1.6/CS1.6TempDecalWAD.bat
+++ Counter-Strike 1.6/CS1.6TempDecalWAD.bat
@@ -1,7 +1,10 @@
 REM Deletes tempdecal.wad from cstrike directory and makes it READ-ONLY
+REM Make sure tempdecal.wad is READ-ONLY and blank 0KB, unless using a custom spray
 
 cd C:\Program Files (x86)\Steam\steamapps\common\Half-Life\cstrike
 
 del tempdecal.wad
 echo. 2>tempdecal.wad
 attrib +r tempdecal.wad
+
+PAUSE
