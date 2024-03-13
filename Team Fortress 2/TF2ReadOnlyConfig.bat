76ccdc1ab3b4e275bf6d695e5c6517bb75f9ebdc
shoober420 <shoober420@gmail.com>
2024-03-12 19:17:55 -0700

    Update Team Fortress 2

---
+++ Team Fortress 2/TF2ReadOnlyConfig.bat
@@ -0,0 +1,18 @@
+REM Makes config files READ ONLY
+REM Prevents admin slowhacking
+
+REM For clean configs:
+REM 1. delete config.cfg and autoexec.cfg
+REM 2. add new autoexec.cfg
+REM 3. run game so that settings are loaded from autoexec.cfg to new clean config.cfg
+REM 4. finally run this batch script to prevent any other external modifications to config files
+
+cd "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf\cfg"
+
+attrib +r config.cfg
+attrib +r autoexec.cfg
+attrib +r autoexecHQ.cfg
+attrib +r autoexecPERF.cfg
+attrib +r autoexecPRO.cfg
+
+PAUSE
