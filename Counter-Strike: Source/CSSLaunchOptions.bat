1142b994377f9755bf054b730a3429965e388f85
shoober420 <shoober420@gmail.com>
2025-09-12 11:21:09 -0400

    Update CSSLaunchOptions.bat

--- Counter-Strike: Source/CSSLaunchOptions.bat
+++ Counter-Strike: Source/CSSLaunchOptions.bat
@@ -1,22 +1,22 @@
 -vulkan -w 3840 -h 2160 -clientport 65007 -console -forcenovsync -fullscreen -limitvsconst -softparticlesdefaultoff -precachefontchars -noff -noforcemaccel -noforcemspd -nohltv -nojoy -nosteamcontroller -novid -preload -refresh 240 -tickrate 128 -nodns -no-browser -noaafonts -nomsaa -noipx +mat_queue_mode 2 -nomessagebox -nocrashdialog -nostartupsound +exec autoexec.cfg
 
--condebug (creates console log, useful for cvarlist generation)
+rem # -condebug (creates console log, useful for cvarlist generation)
 
--limitvsconst will limit the game's number of vertex shaders to a maximum of 256
+rem # -limitvsconst will limit the game's number of vertex shaders to a maximum of 256
 
--high LOWERS FPS
+rem # -high LOWERS FPS
 
--threads: Let the engine decide how many threads it spawns, by omitting the threads launch option (Valve confirmed)
- + If threads is not set, the number of threads in the global thread pool is 'number of logical processors - 2', unless that value is larger than 3 (in which case 3 is the number of threads)
+rem # -threads: Let the engine decide how many threads it spawns, by omitting the threads launch option (Valve confirmed)
+rem # + If threads is not set, the number of threads in the global thread pool is 'number of logical processors - 2', unless that value is larger than 3 (in which case 3 is the number of threads)
 
--d3dex for HQ D3D
+rem # -d3dex for HQ D3D
 
--refreshrate 240 -freq 240 also force refresh rate
+rem # -refreshrate 240 -freq 240 also force refresh rate
 
--vulkan uses DXVK for Source1 games
+rem # -vulkan uses DXVK for Source1 games
 
--vulkan DXVK NOT supported in CS:S yet
+rem # -vulkan DXVK NOT supported in CS:S yet
 
-DXVK can be updated on Windows by swapping old bin/dxvk_d3d9.dll file with new DXVK x32/d3d.dll and matching the original dll name.
+rem # DXVK can be updated on Windows by swapping old bin/dxvk_d3d9.dll file with new DXVK x32/d3d.dll and matching the original dll name.
 
-DXVK can be updated on Linux by compiling latest git master and swapping files.
+rem # DXVK can be updated on Linux by compiling latest git master and swapping files.
